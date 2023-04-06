ae216a752a0fd9f33952cd8b056e63a75a385999
yuki-nishimoto1109 <126940102+yuki-nishimoto1109@users.noreply.github.com>
2023-04-06 10:02:37 +0000

    [Finish] P2-2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @mybook = Book.find(params[:id])
+    @user = @mybook.user
+    @book = Book.new()
+  end
+
+  def index
+    @book = Book.new()
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "You have destroyed book successfully."
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+end
