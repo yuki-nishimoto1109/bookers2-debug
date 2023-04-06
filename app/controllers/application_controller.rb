aac982e6a36c2e7603b1aab89f0887283050981c
yuki-nishimoto1109 <126940102+yuki-nishimoto1109@users.noreply.github.com>
2023-04-06 15:54:20 +0000

    [Add] いいね機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,6 @@ class ApplicationController < ActionController::Base
     user_path(current_user.id)
   end
 
-  # def after_sign_out_path_for(resource)
-  #   root_path
-  # end
-
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create,:destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
